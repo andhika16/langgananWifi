86f79d96565702d57329fb086bb1ce0bd28bc647
andhika16 <moh.andhika16@gmail.com>
2021-07-30 17:28:49 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const slide = document.querySelector('nav ul');
+
+
+menuToggle.addEventListener('click', function () {
+    
+    slide.classList.toggle('slide')
+    
+
+})
+
